a63c20673359737b1c4d83b4c0c55b705b8cca53
Roman Savenko <rsaven.wk@gmail.com>
2024-09-20 14:40:39 +0300

    feat: add random key provider

---
+++ RockPaperScissorsGame/KeyProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace RockPaperScissorsGame;
+
+public static class KeyProvider
+{
+    public static string GenerateRandomSecureKey256()
+    {
+        var keySize = 32;
+        var key = new byte[keySize];
+
+        using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+        rng.GetBytes(key);
+
+        return BitConverter.ToString(key).Replace("-", "").ToLower();
+    }
+}
