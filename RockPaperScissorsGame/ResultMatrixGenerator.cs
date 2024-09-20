106f39a00fbdba34f8becd023c5c5fe9b7eb444f
Roman Savenko <rsaven.wk@gmail.com>
2024-09-20 14:41:37 +0300

    feat: add result matrix generator

---
+++ RockPaperScissorsGame/ResultMatrixGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace RockPaperScissorsGame;
+
+public static class ResultMatrixGenerator
+{
+    public static string[,] GenerateResultMatrix(int moves)
+    {
+        var p = moves / 2;
+        var result = new string[moves, moves];
+
+        for (int i = 0; i < moves; i++)
+        {
+            for(int j = 0; j < moves; j++)
+            {
+                var temp = Math.Sign((i - j + p + moves) % moves - p); 
+                result[i, j] = ResultToStrFormat(temp);
+            }
+        }
+
+        return result;
+    }
+
+    private static string ResultToStrFormat(int result)
+    {
+        return result switch
+        {
+            1 => "Win",
+            -1 => "Lose",
+            0 => "Draw",
+            _ => "Invalid result"
+        };
+    }
+
+}
