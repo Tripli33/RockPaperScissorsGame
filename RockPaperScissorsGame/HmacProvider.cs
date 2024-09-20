439f0536650367279cec056055ea2554056bf47e
Roman Savenko <rsaven.wk@gmail.com>
2024-09-20 14:41:19 +0300

    feat: add hmac provider

---
+++ RockPaperScissorsGame/HmacProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RockPaperScissorsGame;
+
+public static class HmacProvider
+{
+    public static string GenerateHmacWithSha256(string secureKey, string message)
+    {
+        var keyBytes = Encoding.UTF8.GetBytes(secureKey);
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+
+        using HMACSHA256 hmac = new(keyBytes);
+        var hashMessage = hmac.ComputeHash(messageBytes);
+
+        return BitConverter.ToString(hashMessage).Replace("-", "").ToLower();
+    }
+}
