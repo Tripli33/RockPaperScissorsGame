b8195cfc337f9c71d5722da563ad127a00ceacff
Roman Savenko <rsaven.wk@gmail.com>
2024-09-20 14:42:32 +0300

    feat: add table generator

---
+++ RockPaperScissorsGame/TableGenerator.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleTables;
+
+namespace RockPaperScissorsGame;
+
+public static class TableGenerator
+{
+    public static ConsoleTable GenerateTable(string[] args, string[,] resultMatrix)
+    {
+        var table = new ConsoleTable("v PC\\User > ").AddColumn(args);
+        var temp = new string[args.Length + 1];
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            temp[0] = args[i];
+            for (int j = 0; j < args.Length; j++)
+            {
+                temp[j + 1] = resultMatrix[i, j];
+            }
+
+            table.AddRow(temp.ToArray());
+        }
+
+        return table;
+    }
+}
