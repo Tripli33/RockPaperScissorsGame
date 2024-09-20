4215dde85afdc63bcf1a49c481f1e81862185a13
Roman Savenko <rsaven.wk@gmail.com>
2024-09-20 14:42:56 +0300

    feat: add validator for command line args

---
+++ RockPaperScissorsGame/Validator.cs
@@ -0,0 +1,17 @@
+﻿namespace RockPaperScissorsGame;
+
+public static class Validator
+{
+    public static void ValidateArgs(string[] args)
+    {
+        if (args.Length < 3 || args.Length % 2 == 0)
+        {
+            throw new ArgumentException("Moves numbers must be greater than 3 and only odd");
+        }
+
+        if (args.Distinct().Count() != args.Length)
+        {
+            throw new ArgumentException("Moves must be unique.");
+        }
+    }
+}
